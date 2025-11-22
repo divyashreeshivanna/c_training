731c8786f8acda8ff2951704b3bead3aafb873dd
DIVYASHREE SHIVANNA <divyashivannar@gmail.com>
2025-11-22 10:40:56 +0530

    Add files via upload

---
+++ vcube.c
@@ -0,0 +1,13 @@
+//write a c function which will accept three parameters l,b,h and writterns volume of the cube//
+#include<stdio.h>
+int volume(int l,int b, int h)
+{
+    int volume=l*b*h;
+}
+void main()
+{
+int l=9;
+int b=8;
+int h=7;
+printf("%d\n",volume(l,b,h));
+}
